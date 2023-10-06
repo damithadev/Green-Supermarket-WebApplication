e167fd822861b83cda53ecab707ed299b09b8483
Damitha <hellodamitha@gmail.com>
2023-10-06 18:36:35 +0530

    Can request GET and POST methods

--- src/main/java/com/example/webproject/HelloServlet.java
+++ src/main/java/com/example/webproject/HelloServlet.java
@@ -1,27 +1,42 @@
 package com.example.webproject;
 
 import java.io.*;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
+//    private String message;
+//
+//    public void init() {
+//        message = "Hello World!";
+//    }
+//
+//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html");
+//
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+//    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        System.out.println("Hello to the GET req from servlet");
+
+        resp.setContentType("text/html");
+
+        resp.getWriter().println("<h1>Hello there </h1>");
+
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        System.out.println("Hello to the POST req from servlet");
     }
 
-    public void destroy() {
-    }
 }
