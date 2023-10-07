33bca60036652d41c609d4b8a1d961d3fc948d55
Damitha <hellodamitha@gmail.com>
2023-10-07 23:12:06 +0530

    1st servlet name changed

--- src/main/java/com/example/webproject/register.java
+++ src/main/java/com/example/webproject/register.java
@@ -7,25 +7,8 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
-
-//    private String message;
-//
-//    public void init() {
-//        message = "Hello World!";
-//    }
-//
-//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        response.setContentType("text/html");
-//
-//        // Hello
-//        PrintWriter out = response.getWriter();
-//        out.println("<html><body>");
-//        out.println("<h1>" + message + "</h1>");
-//        out.println("</body></html>");
-//    }
-
+@WebServlet(name = "register", value = "/register")
+public class register extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         System.out.println("Hello to the GET req from servlet");
